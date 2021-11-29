387cf769c375804847d70db29c7f7402d9a0ff57
Von Bryan Guron <vonkulit@gmail.com>
2021-11-29 14:43:25 +0800

    Back to original

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
-      home: MyHomePage(title: 'F.I.R.E Assesasdasdawedfadadsasment'),
+      home: MyHomePage(title: 'F.I.R.E Assessment'),
     );
   }
 }
