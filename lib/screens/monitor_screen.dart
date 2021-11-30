63bab6a493c69483d571eba2ee43d892753c8546
BaiWinchester <popcklwinchester@gmail.com>
2021-11-30 13:49:20 +0800

    Gumagana na yung computation, added 1 more screen whcih is result. computation lang nandoon so fix na lang, ty

--- lib/screens/monitor_screen.dart
+++ lib/screens/monitor_screen.dart
@@ -1,3 +1,5 @@
+import 'package:fire_moneytor/functions/calcbrain.dart';
+import 'package:fire_moneytor/screens/result_screen.dart';
 import 'package:flutter/material.dart';
 
 class MonitorScreen extends StatefulWidget {
@@ -10,6 +12,21 @@ class MonitorScreen extends StatefulWidget {
 class _MonitorScreenState extends State<MonitorScreen> {
   @override
   Widget build(BuildContext context) {
+    TextEditingController _spendingController = new TextEditingController();
+    TextEditingController _incomeController = new TextEditingController();
+    TextEditingController _savingsController = new TextEditingController();
+    TextEditingController _ageController = new TextEditingController();
+    double income = 0.0;
+    double mExpenses = 0.0;
+    double savings = 0.0;
+    int age = 0;
+    void dispose() {
+      _spendingController.dispose();
+      _incomeController.dispose();
+      _savingsController.dispose();
+      _ageController.dispose();
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Monitor'),
@@ -18,21 +35,62 @@ class _MonitorScreenState extends State<MonitorScreen> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
+          children: <Widget>[
             Text(
               '''
-              Input your monthly expenses here.
+              Input your monthly expenses,savings, income, and age here.
               
               ''',
             ),
             SizedBox(
               width: 290,
               child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter your monthly expenses here.',
-                    contentPadding: EdgeInsets.all(20.0),
-                  ),
+                controller: _spendingController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter your monthly expenses here.',
+                  contentPadding: EdgeInsets.all(20.0),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 290,
+              child: TextField(
+                controller: _savingsController,
+                keyboardType: TextInputType.number,
+                // Only numbers can be entered
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter your savings here.',
+                  contentPadding: EdgeInsets.all(20.0),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 290,
+              child: TextField(
+                controller: _incomeController,
+                keyboardType: TextInputType.number,
+                // Only numbers can be entered
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter your income here.',
+                  contentPadding: EdgeInsets.all(20.0),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 290,
+              child: TextField(
+                controller: _ageController,
+                keyboardType: TextInputType.number,
+                // Only numbers can be entered
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter your age here.',
+                  contentPadding: EdgeInsets.all(20.0),
+                ),
               ),
             ),
           ],
@@ -42,7 +100,29 @@ class _MonitorScreenState extends State<MonitorScreen> {
         width: 70.0,
         height: 50.0,
         child: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            income = double.parse(_incomeController.value.text);
+            mExpenses = double.parse(_spendingController.value.text);
+            savings = double.parse(_savingsController.value.text);
+            age = int.parse(_ageController.value.text);
+            print("income: $income");
+            print("Monthly Expenses: $mExpenses");
+            print("Savings: $savings");
+            print("Age: $age");
+            CalcBrain calcu = new CalcBrain(
+                age: age,
+                monthlyexpense: mExpenses,
+                savings: savings,
+                monthlyincome: income);
+            calcu.calculateAnnualExpense();
+            calcu.calculateAnnualIncome();
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => ResultPage(
+                        age: calcu.calculateAge(),
+                        fire: calcu.calculateFire())));
+          },
           backgroundColor: const Color(0xFF2CDB30),
           shape: const BeveledRectangleBorder(
             borderRadius: BorderRadius.zero,
@@ -55,7 +135,6 @@ class _MonitorScreenState extends State<MonitorScreen> {
     );
   }
 }
-
 
 /* Savings
 class _MonitorScreenState extends State<MonitorScreen> {
@@ -73,7 +152,7 @@ class _MonitorScreenState extends State<MonitorScreen> {
             Text(
               '''
               Input your savings here.
-              
+
               ''',
             ),
             SizedBox(
